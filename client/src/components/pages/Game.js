21a40db266e72435bb7ca94865cf791f4d0b6a1f
Brent <brentliu02@gmail.com>
2021-01-19 08:55:24 -0800

    blobs can now infect

--- client/src/components/pages/Game.js
+++ client/src/components/pages/Game.js
@@ -16,6 +16,45 @@ class Game extends Component {
             y: 50,
             infected: Math.random() < 0.5,
         };
+        this.handleKeyDown = this.handleKeyDown.bind(this);
+    }
+
+    handleKeyDown(event) {
+        if (event.key === "w" || event.key === "a" || event.key === "s" || event.key === "d") {
+            this.setState((state) => {
+                switch (event.key) {
+                    case "w": return ({y: state.y - 1})
+                    case "a": return ({x: state.x - 1})
+                    case "s": return ({y: state.y + 1})
+                    case "d": return ({x: state.x + 1})
+                }
+            }, () => {
+                let userId = ""
+                if (this.props.userId) {
+                    userId = this.props.userId.toString();
+                }
+                let blob = {
+                    player_id: userId,
+                    x: this.state.x,
+                    y: this.state.y,
+                    infected: this.state.infected,
+                }
+
+                for (let i = 0; i < this.state.blobs.length; i++) {
+                    let otherBlob = this.state.blobs[i];
+                    if ((otherBlob.infected ^ blob.infected) && ((blob.x - otherBlob.x) ** 2 + (blob.y - otherBlob.y) ** 2 < 5)){
+                        if (!otherBlob.infected) {
+                            otherBlob.infected = true;
+                            post("/api/blob", otherBlob);
+                        }
+                        blob.infected = true;
+                    }
+                }
+
+                console.log("calling api blob");
+                post("/api/blob", blob);
+            })
+        }
     }
 
     componentDidMount() {
@@ -23,6 +62,16 @@ class Game extends Component {
 
         get("/api/blobs").then((blobs) => {
             this.setState({blobs: blobs});
+            /*for (let i = 0; i < blobs.length; i++) {
+                const blob = blobs[i];
+                if (blob.player_id === this.props.userId) {
+                    this.setState({
+                        x: blob.x,
+                        y: blob.y,
+                        infected: blob.infected,
+                    })
+                }
+            }*/
         })
 
         socket.on("blob", (data) => {
@@ -39,37 +88,13 @@ class Game extends Component {
             }
         })
 
-        window.addEventListener('keydown', (event) => {
-            if (event.key === "w" || event.key === "a" || event.key === "s" || event.key === "d") {
-                this.setState((state) => {
-                    switch (event.key) {
-                        case "w": return ({y: state.y - 1})
-                        case "a": return ({x: state.x - 1})
-                        case "s": return ({y: state.y + 1})
-                        case "d": return ({x: state.x + 1})
-                    }
-                }, () => {
-                    let userId = ""
-                    if (this.props.userId) {
-                        userId = this.props.userId.toString();
-                    }
-                    let blob = {
-                        player_id: userId,
-                        x: this.state.x,
-                        y: this.state.y,
-                        infected: this.state.infected,
-                    }
-                    console.log("calling api blob");
-                    post("/api/blob", blob);
-                })
-            }
-        })
+        window.addEventListener('keydown', this.handleKeyDown)
     }
 
     BlobDisplay = ({blobs}) => (
         <div>
             {blobs.map(blob => (
-                <Blob x={blob.x} y={blob.y} infected={blob.infected}></Blob>
+                <Blob key={blob.player_id} x={blob.x} y={blob.y} infected={blob.infected}></Blob>
             ))}
         </div>
     )
@@ -81,6 +106,11 @@ class Game extends Component {
                 <Blob x={this.state.x} y={this.state.y} infected={this.state.infected}></Blob>
             </div>
         )
+    }
+
+    componentWillUnmount() {
+        console.log("enter componentWillUnmount");
+        window.removeEventListener('keydown', this.handleKeyDown);
     }
 }
 
