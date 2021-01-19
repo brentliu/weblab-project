7bf4d91f89781d8dc0f7fd9a41c68ca84e2c5628
Brent <brentliu02@gmail.com>
2021-01-19 00:33:41 -0800

    game page that reads from database

--- README.md
+++ README.md
@@ -1,8 +1,10 @@
-# Workshop 1 Conclusion!
+# catbook-react
 
-## to start
+## start up
 
-Run `npx webpack` to build your react code! Then open up `index.html` in a browser of your choice.
+run `npm start` in one terminal and `npm run hotloader` in another
+
+visit `http://localhost:5000`
 
 ## don't touch
 
@@ -12,7 +14,9 @@ the following files students do not need to edit. feel free to read them if you 
 client/dist/index.html
 client/src/index.js
 client/src/utilities.js
+client/src/client-socket.js
 server/validator.js
+server/server-socket.js
 .babelrc
 .npmrc
 .prettierrc
--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,6 +1,15 @@
 import React, { Component } from "react";
 import NavBar from "./modules/NavBar.js";
+import { Router } from "@reach/router";
+import Feed from "./pages/Feed.js";
+import NotFound from "./pages/NotFound.js";
 import Profile from "./pages/Profile.js";
+import Chatbook from "./pages/Chatbook.js";
+import Game from "./pages/Game.js";
+
+import { socket } from "../client-socket.js";
+
+import { get, post } from "../utilities";
 
 // to use styles, import the necessary CSS files
 import "../utilities.css";
@@ -10,6 +19,39 @@ import "./App.css";
  * Define the "App" component as a class.
  */
 class App extends Component {
+  // makes props available in this component
+  constructor(props) {
+    super(props);
+    this.state = {
+      userId: undefined,
+    };
+  }
+
+  componentDidMount() {
+    get("/api/whoami").then((user) => {
+      if (user._id) {
+        // they are registed in the database, and currently logged in.
+        this.setState({ userId: user._id });
+      }
+    });
+    // TODO (extra!): Catch the "forceDisconnect" socket event
+    // in the callback function, set the state 'socketDisconnected' to true
+  }
+
+  handleLogin = (res) => {
+    console.log(`Logged in as ${res.profileObj.name}`);
+    const userToken = res.tokenObj.id_token;
+    post("/api/login", { token: userToken }).then((user) => {
+      this.setState({ userId: user._id });
+      post("/api/initsocket", { socketid: socket.id });
+    });
+  };
+
+  handleLogout = () => {
+    this.setState({ userId: undefined });
+    post("/api/logout");
+  };
+
   // required method: whatever is returned defines what
   // shows up on screen
   render() {
@@ -17,9 +59,19 @@ class App extends Component {
       // <> is like a <div>, but won't show
       // up in the DOM tree
       <>
-        <NavBar />
+        <NavBar
+          handleLogin={this.handleLogin}
+          handleLogout={this.handleLogout}
+          userId={this.state.userId}
+        />
         <div className="App-container">
-          <Profile />
+          <Router>
+            <Feed path="/" userId={this.state.userId} />
+            <Profile path="/profile/:userId" />
+            <Chatbook path="/chat/" userId={this.state.userId} />
+            <Game path="/game/" userId={this.state.userId}/>
+            <NotFound default />
+          </Router>
         </div>
       </>
     );
