a256aaea89f8d922ad277d6b55ca554c064f2ab3
Brent <brentliu02@gmail.com>
2021-01-19 02:07:41 -0800

    .

--- server/server.js
+++ server/server.js
@@ -58,7 +58,7 @@ app.use(bodyParser.json());
 // set up a session, which will persist login data across requests
 app.use(
   session({
-    secret: "session-secret",
+    secret: process.env.SESSION_SECRET,
     resave: false,
     saveUninitialized: false,
   })
