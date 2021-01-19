cf79b9eecb6b270b77ed0873dd29b3f5262c118c
Brent <brentliu02@gmail.com>
2021-01-19 03:00:04 -0800

    change to own google client id

--- client/src/components/modules/NavBar.js
+++ client/src/components/modules/NavBar.js
@@ -5,7 +5,7 @@ import GoogleLogin, { GoogleLogout } from "react-google-login";
 import "./NavBar.css";
 
 // This identifies your web application to Google's authentication service
-const GOOGLE_CLIENT_ID = "121479668229-t5j82jrbi9oejh7c8avada226s75bopn.apps.googleusercontent.com";
+const GOOGLE_CLIENT_ID = "374021644318-2roplhiav87un4nks3hjlpa3001v0eq3.apps.googleusercontent.com";
 
 /**
  * The navigation bar at the top of all pages. Takes no props.
